337f4ee9c285af747d243d391572abcb0c31641a
gnix0 <dev.gustavoa@gmail.com>
2026-07-17 14:00:01 -0300

    chore: add functions to la.h

--- include/la.h
+++ include/la.h
@@ -6,4 +6,16 @@ typedef struct {
     double y_axis;
 } Vec2f;
 
+Vec2f vec_create(double x, double y);
+Vec2f vec_scale(Vec2f v, double s);
+Vec2f vec_div_scalar(Vec2f v, double s);
+Vec2f vec_mul(Vec2f v1, Vec2f v2);
+Vec2f vec_div(Vec2f v1, Vec2f v2);
+Vec2f vec_add(Vec2f v1, Vec2f v2);
+Vec2f vec_dub(Vec2f v1, Vec2f v2);
+Vec2f vec_add_assign(Vec2f *v1, Vec2f v2);
+Vec2f vec_sub_assign(Vec2f *v1, Vec2f v2);
+double vec_length(Vec2f v);
+Vec2f vec_normalize(Vec2f v);
+
 #endif
